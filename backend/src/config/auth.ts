45bb303fdf1564f689fbd8b253ecd86d6a2cf248
guilherme-teixeira-gomes <guilherme.tgomes@hotmail.com>
2024-10-26 10:25:57 -0300

    fix

--- backend/src/config/auth.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-export default {
-  secret: process.env.JWT_SECRET || "mysecret",
-  expiresIn: "1d",
-  refreshSecret: process.env.JWT_REFRESH_SECRET || "mysecret",
-  refreshExpiresIn: "7d"
-};
